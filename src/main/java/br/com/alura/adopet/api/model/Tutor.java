3652a763e5e24f3e755de4f6af75b297bc6b584c
Luan Valente <luanbvalente@hotmail.com>
2024-01-07 14:26:50 -0300

    Refatoração Tutor

--- src/main/java/br/com/alura/adopet/api/model/Tutor.java
+++ src/main/java/br/com/alura/adopet/api/model/Tutor.java
@@ -1,10 +1,8 @@
 package br.com.alura.adopet.api.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import br.com.alura.adopet.api.dto.AtualizacaoTutorDto;
+import br.com.alura.adopet.api.dto.CadastroTutorDto;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 import java.util.Objects;
@@ -17,19 +15,29 @@ public class Tutor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
     private String nome;
 
-    @NotBlank
-    @Pattern(regexp = "\\(?\\d{2}\\)?\\d?\\d{4}-?\\d{4}")
     private String telefone;
 
-    @NotBlank
-    @Email
     private String email;
 
     @OneToMany(mappedBy = "tutor")
     private List<Adocao> adocoes;
+
+    public Tutor() {
+    }
+
+    public Tutor(CadastroTutorDto dto) {
+        this.nome = dto.nome();
+        this.telefone = dto.telefone();
+        this.email = dto.email();
+    }
+
+    public void atualizarDados(AtualizacaoTutorDto dto) {
+        this.nome = dto.nome();
+        this.telefone = dto.telefone();
+        this.email = dto.email();
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -48,32 +56,16 @@ public class Tutor {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getTelefone() {
         return telefone;
     }
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public List<Adocao> getAdocoes() {
---
+++ src/main/java/br/com/alura/adopet/api/service/TutorService.java
@@ -0,0 +1,28 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.AtualizacaoTutorDto;
+import br.com.alura.adopet.api.dto.CadastroTutorDto;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TutorService {
+
+    @Autowired
+    private TutorRepository repository;
+
+    public void cadastrar(CadastroTutorDto dto) {
+        boolean jaCadastrado = repository.existsByTelefoneOrEmail(dto.telefone(), dto.email());
+
+        if (jaCadastrado) {
+            throw new RuntimeException("Dados já cadastrados para outro tutor!");
+        } else {
+            repository.save(new Tutor(dto));
+        }
+    }
+
+    public void atualizar(AtualizacaoTutorDto dto) {
+        Tutor tutor = repository.getReferenceById(dto.id());
+        tutor.atualizarDados(dto);
+    }
+}
